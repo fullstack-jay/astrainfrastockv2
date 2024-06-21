c288f5c408365c79b1e045241d142490866efc00
Rizqi Reza Ardiansyah <rizqirezaardiansyah@gmail.com>
2024-06-21 11:23:30 +0700

    Initial Commit

---
+++ component/delete/delete_master.php
@@ -0,0 +1,49 @@
+<?php
+include "../../configuration/config_connect.php";
+include "../../configuration/config_session.php";
+include "../../configuration/config_etc.php";
+$forward =$_GET['forward'];
+$no = $_GET['no'];
+$forwardpage = $_GET['forwardpage'];
+?>
+
+<?php
+if($_SESSION['jabatan'] =='admin'){
+
+ $sql = "delete from $forward where no='".$no."'";
+ if (mysqli_query($conn, $sql)) {
+ ?>
+
+  <body onload="setTimeout(function() { document.frm1.submit() }, 10)">
+  <form action="<?php echo $baseurl; ?>/<?php echo $forwardpage;?>" name="frm1" method="post">
+
+  <input type="hidden" name="hapusberhasil" value="1" />
+
+<?php
+ } else{
+ ?>   <body onload="setTimeout(function() { document.frm1.submit() }, 10)">
+	  <input type="hidden" name="hapusberhasil" value="2" />
+ <?php
+ }
+}
+else{
+
+ ?>
+  <body onload="setTimeout(function() { document.frm1.submit() }, 10)">
+   <form action="<?php echo $baseurl; ?>/<?php echo $forwardpage;?>" name="frm1" method="post">
+
+
+	  <input type="hidden" name="hapusberhasil" value="2" />
+ <?php
+ }
+?>
+<table width="100%" align="center" cellspacing="0">
+  <tr>
+    <td height="500px" align="center" valign="middle"><img src="../../dist/img/load.gif">
+  </tr>
+</table>
+
+
+   </form>
+<meta http-equiv="refresh" content="10;url=jump?forward=<?php echo $forward.'&';?>forwardpage=<?php echo $forwardpage.'&'; ?>chmod=<?php echo $chmod; ?>">
+</body>
